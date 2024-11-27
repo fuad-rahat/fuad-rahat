e1ca6db2493fada9904193c8abed97b0ed252bf1
rahatmf <rahatmf@gmail.com>
2024-11-27 22:20:31 +0600

    changing theme and fixing build bug

--- vite.config.js
+++ vite.config.js
@@ -8,19 +8,8 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000, // Increases to 1MB to avoid warnings for slightly larger chunks
 
     rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Check if the file is in node_modules
-          if (id.includes('node_modules')) {
-            const packageName = id.split('node_modules/')[1].split('/')[0];
-            if (['react', 'react-dom'].includes(packageName)) {
-              return 'react-vendor'; // React code goes into a separate chunk
-            }
-            return 'vendor'; // Other third-party code goes into a "vendor" chunk
-          }
-        },
-      },
-    },
+      external: ['path/to/homecss.css']
+    }
     
   },
 })
